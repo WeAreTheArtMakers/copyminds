09a7d5729af3c987fcd7a64cc2c1bbd78356f6a0
WATAM <113291421+WeAreTheArtMakers@users.noreply.github.com>
2025-05-30 08:37:22 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,14 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// public içindekileri her istekte sun:
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ana sayfada app.html’i otomatik göster:
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'app.html'));
+});
+
 app.use('/api/user', userRoutes);
 app.use('/api/copytrade', copytradeRoutes);
 
